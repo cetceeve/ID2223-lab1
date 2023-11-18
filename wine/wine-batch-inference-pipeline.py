d7304be036b8f755ea932a992d95f0e28e8023f2
Fabian Zeiher <fzeiher@gmail.com>
2023-11-18 20:10:15 +0100

    finished batch inference for wine dataset

---
+++ wine/wine-batch-inference-pipeline.py
@@ -0,0 +1,106 @@
+import os
+import modal
+import numpy as np
+    
+LOCAL=True
+
+if LOCAL == False:
+   stub = modal.Stub()
+   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks","joblib","seaborn","sklearn==1.1.1","dataframe-image"])
+   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+   def f():
+       g()
+
+def g():
+    import pandas as pd
+    import hopsworks
+    import joblib
+    import datetime
+    from PIL import Image
+    from datetime import datetime
+    import dataframe_image as dfi
+    from sklearn.metrics import confusion_matrix
+    from matplotlib import pyplot
+    import seaborn as sns
+
+    project = hopsworks.login(project="zeihers_mart")
+    fs = project.get_feature_store()
+    
+    mr = project.get_model_registry()
+    model = mr.get_model("wine_model", version=3)
+    model_dir = model.download()
+    model = joblib.load(model_dir + "/wine_model.pkl")
+    
+    feature_view = fs.get_feature_view(name="wine", version=1)
+    batch_data = feature_view.get_batch_data()
+    
+    y_pred = model.predict(batch_data)
+    # offset for the newly generated batch
+    offset = 10
+
+    new_pred = y_pred[-offset:]
+    new_pred = new_pred + 3
+    # print(new_pred)
+    
+    # read the actual label
+    wine_fg = fs.get_feature_group(name="wine", version=1)
+    df = wine_fg.read() 
+
+    now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    data = {
+        'prediction': new_pred,
+        'label': df.tail(offset)["quality"],
+        'datetime': np.full(offset, now),
+        'index': df.tail(offset)["index"],
+    }
+
+    monitor_df = pd.DataFrame(data)
+    print(monitor_df)
+
+    # feature group for monitoring 
+    monitor_fg = fs.get_or_create_feature_group(name="wine_predictions",
+                                                version=1,
+                                                primary_key=["index"],
+                                                description="Wine quality Prediction/Outcome Monitoring"
+                                                )  
+    # insert the new prediction results for the generated data
+    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
+    
+    history_df = monitor_fg.read()
+    # Add our prediction to the history, as the history_df won't have it - 
+    # the insertion was done asynchronously, so it will take ~1 min to land on App
+    history_df = pd.concat([history_df, monitor_df])
+
+    df_recent = history_df.tail(40)
+    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
+
+    dataset_api = project.get_dataset_api()    
+    dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
+    
+    predictions = history_df[['prediction']]
+    labels = history_df[['label']]
+
+    # Only create the confusion matrix when our wine_predictions feature group has examples of at least 4wine qualities
+    print("Number of different wine predictions to date: " + str(predictions.value_counts().count()))
+    if predictions.value_counts().count() > 4:
+        results = confusion_matrix(labels, predictions, labels=range(4, 9))
+    
+        df_cm = pd.DataFrame(results, [f"True: {s}" for s in range(4, 9)],
+                             [f"Pred: {s}" for s in range(4, 9)])
+    
+        cm = sns.heatmap(df_cm, annot=True)
+        fig = cm.get_figure()
+        fig.savefig("./confusion_matrix.png")
+        dataset_api.upload("./confusion_matrix.png", "Resources/images", overwrite=True)
+    else:
+        print("You need at least 4 different wine predictions to create the confusion matrix.")
+        print("Run the batch inference pipeline more times until you get at least 4 different wine predictions") 
+
+
+if __name__ == "__main__":
+    if LOCAL == True :
+        g()
+    else:
+        with stub.run():
+            f()
+
